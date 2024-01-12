5e883f01e975a905b2e69d618b0e2e20d2dc2679
venkateshwebdev <nikhilsirigineedi3@gmail.com>
2024-01-12 17:38:48 +0530

    done

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
+import { GiSettingsKnobs } from "react-icons/gi";
+import { VscBellDot } from "react-icons/vsc";
+import { TiInfoLarge } from "react-icons/ti";
 const Navbar = () => {
     return (
         <div className="w-full flex items-center justify-between px-[4%] py-[2%] relative">
-            <Image src={"/Ishaare.com.png"} height={100} width={100} alt='isharee' />
-            <div className="flex ">
-                {/* <Image src={"/Group.png"} className="" height={20} width={20} alt="" />
-                <Image src={"/Icon.png"} className="" height={20} width={20} alt="" />
-                <Image src={"/Notification.png"} className="" height={20} width={20} alt="" /> */}
+            <Image src={"/Ishaare.com.png"} height={48} width={173} alt='isharee' />
+            <div className="flex items-center justify-between gap-6 max-md:gap-3">
+                <GiSettingsKnobs className="rotate-90 max-md:text-[24px] text-[32px] font-bold " />
+                
+                <TiInfoLarge className="text-[32px] max-md:text-[24px] font-bold" />
+                <VscBellDot className="text-[32px] max-md:text-[24px] font-bold" />
+                <div className="p-2 rounded-full border-[1px] border-[#EE9813] bg-[#FAF3DC]">NV</div>
             </div>
         </div>
     )
